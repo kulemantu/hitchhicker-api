1693dd07141193eb371094ed87b896a9ee9258f5
Kago Kagichiri <kago.kagichiri@gmail.com>
2023-11-19 23:03:53 +0300

    feat(port): allow port 80, fix default route
    Signed-off-by: Kago Kagichiri <kago.kagichiri@gmail.com>

--- MetricsLocal/app.py
+++ MetricsLocal/app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Welcome to MetricsCentral!'
+def home():
+    return 'Welcome to MetricsLocal!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
